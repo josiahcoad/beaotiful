d2f93086ca98331eb396f8824f75bcff719a1379
Josiah Coad <josiah@coad.net>
2022-03-01 00:09:44 -0600

    bug fix

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 def go_to_link(url):
     js = f"window.open('{url}')"  # New tab or window
-    js = f"window.location.href = '{url}'"  # Current tab
+    # js = f"window.location.href = '{url}'"  # Current tab
     html = '<img src onerror="{}">'.format(js)
     div = Div(text=html)
     st.bokeh_chart(div)
