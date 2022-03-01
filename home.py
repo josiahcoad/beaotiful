b3eb80036296debd8382a5af0ec6a613f5c4245d
Josiah Coad <josiah@coad.net>
2022-03-01 00:06:21 -0600

    bug fix

--- home.py
+++ home.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import requests
+API_ENDPOINT = 'https://c4s1pb1xef.execute-api.us-west-2.amazonaws.com/api/'
 
 # todo: integrate stripe invoice page
 # todo: select premades that autofill the checkboxes
 
-# todo: use user info to query api for recommended macros. Show stacked bar plot that shows current selection against 
+# todo: use user info to query api for recommended macros. Show stacked bar plot that shows current selection against
 # todo: add settings page to edit your address, health details, etc
 # todo: consider saving user purchase history
 # todo: integrate recommendations via ordering the options
@@ -23,7 +25,8 @@ import numpy as np
 # todo: show price per service instead of servings
 
 st.title('Beoatiful Order Form')
-categories = ["🥣 base", "🍓 fruit", "🥜 nuts", "🌻 seeds", "💥 powder", "💪 protein", "🍫 treats", "🍯 sweetener"]
+categories = ["🥣 base", "🍓 fruit", "🥜 nuts", "🌻 seeds",
+              "💥 powder", "💪 protein", "🍫 treats", "🍯 sweetener"]
 df = pd.read_csv('nutritional_benefits.csv')
 micronutrients = pd.read_csv('micronutrients.csv').set_index('item')
 
@@ -32,29 +35,49 @@ df_ = df.set_index('item')
 checkboxes = {}
 col1, col2 = st.columns((1, 2))
 
-with col1:
-    # subtitle = st.text('1. Choose a base')
-    # base = categories[0]
-    # base = st.radio(base, groups[base])
-    # subtitle = st.text('2. Choose your toppings')
 
-    for category in categories:
+def make_int_if_whole(number):
+    return int(number) if int(number) == number else number
+
+
+with col1:
+    base = categories[0]
+    st.text(f"{base.capitalize()} (choose 1)")
+
+    oats = groups[base][0]
+    size = make_int_if_whole(df_.loc['oats']["size"])
+    benefits = df_.loc[oats].drop(['category', 'cost', 'size'])
+    benefits = ', '.join(benefits[benefits == 1].index)
+    checkboxes[oats] = st.checkbox(
+        f'{oats} ({size} cups, ${df_.loc["oats", "cost"]})', value=False, help=benefits)
+
+
+    granola = groups[base][1]
+    size = make_int_if_whole(df_.loc[granola, "size"])
+    benefits = df_.loc[granola].drop(['category', 'cost', 'size'])
+    benefits = ', '.join(benefits[benefits == 1].index)
+    checkboxes[granola] = st.checkbox(
+        f'{granola} ({size} cups, ${df_.loc[granola, "cost"]})', value=False, help=benefits)
+
+    st.text(f"Now add a ton of toppings!")
+    for category in categories[1:]:
         # todo: add a (1/6) logic to each section
         # todo: pull out the bases from the option logic and make radio button
         selected_num = 0
         with st.expander(f"{category.capitalize()} ({len(df[df.category == category])})"):
             first = df_.loc[groups[category][0]]
-            size = int(first["size"]) if int(first["size"]) == first["size"] else first["size"]
+            size = make_int_if_whole(first["size"])
             st.text(f'{size} cup, ${first["cost"]} each')
             for option in groups[category]:
                 benefits = df_.loc[option].drop(['category', 'cost', 'size'])
                 benefits = ', '.join(benefits[benefits == 1].index)
                 if option in micronutrients.index:
-                    mymicro = micronutrients.loc[option] 
+                    mymicro = micronutrients.loc[option]
                     mymicro = ', '.join(mymicro[mymicro == 1].index)
                 else:
                     mymicro = ''
-                checkboxes[option] = st.checkbox(option, value=False, help=benefits)
+                checkboxes[option] = st.checkbox(
+                    option, value=False, help=benefits)
 
 checkboxes = pd.Series(checkboxes, name='selected')
 joined = df.set_index('item').join(checkboxes)
@@ -64,18 +87,21 @@ with st.sidebar:
     # todo: title chart ( st.altair_chart)
     # todo: make chart y axis ints
     # idea: sort bars greatest to least?
-    benefits = joined[joined.selected == True].drop(['selected', 'category', 'cost', 'size'], axis=1)
+    benefits = joined[joined.selected == True].drop(
+        ['selected', 'category', 'cost', 'size'], axis=1)
     if benefits.sum().sum() == 0:
         st.text('Add some ingredients to see\nyour benefits profile!')
     st.bar_chart(benefits.sum(0))
     total_cost = joined[joined.selected == True].cost.sum()
-    total_servings = int(np.ceil(joined[joined.selected == True]['size'].sum() / (1/2)))
+    total_servings = int(
+        np.ceil(joined[joined.selected == True]['size'].sum() / (1/2)))
     # st.write('<style>.header{padding: 10px 16px; background: #555; color: #f1f1f1; position:fixed;top:0 ;left:0;} .sticky { position: fixed; top: 0; width: 100%;} </style><div class="header" id="myHeader">My Total: $'+str(total_cost)+f'</br>Servings: {total_servings}'+f'</br>Until Free Shipping: ${41-total_cost}</div>', unsafe_allow_html=True)
 
 # with st.sidebar:
     st.text(f'My Total: ${total_cost}')
     if total_servings:
-        st.text(f'Cost Per Serving: ${round(total_cost/total_servings, 2) if total_servings else "":.2f}')
+        st.text(
+            f'Cost Per Serving: ${round(total_cost/total_servings, 2) if total_servings else "":.2f}')
     st.text(f'Until Free Shipping: ${41-total_cost}')
 
 center_col = st.columns(3)[1]
@@ -83,10 +109,9 @@ center_col = st.columns(3)[1]
 with center_col:
     clicked = st.button('Check Out!')
     if clicked:
-        import requests
-        API_ENDPOINT = 'http://127.0.0.1:8000'
-        response = requests.post(f'{API_ENDPOINT}/get_payment_link', json={'oats': 1, 'fruit': 3})
+        response = requests.post(
+            f'{API_ENDPOINT}/get_payment_link', json={'oats': 1, 'fruit': 3})
         print(response.text)
         from utils import go_to_link
-        # go_to_link(response.text)
-        st.markdown("[Click here to complete checkout](%s)" % response.text)
+        go_to_link(response.text)
+        # st.markdown("[Click here to complete checkout](%s)" % response.text)
